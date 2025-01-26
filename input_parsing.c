3d2a45dc923379594246dc26599d3cb7114740a9
unicorn453 <kira.ruseva@gmail.com>
2025-01-26 14:26:44 +0100

    added fix to test 14 regarding return value and unlink improper use

--- input_parsing.c
+++ input_parsing.c
@@ -12,48 +12,58 @@
 
 #include "pipex.h"
 
-void perror_exit(t_parse *parse)
+void	perror_exit(void)
 {
-    free(parse);
-    // perror(name);
-
-   perror("\033[31mError");
-    exit(EXIT_FAILURE);
+	// free(parse);
+	perror("\033[31mError");
+	exit(EXIT_FAILURE);
 }
 
-t_parse *init_parse(char *file, char *commands, bool input)
+t_parse	*init_parse(char *file, char *commands, bool input)
 {
-    t_parse *parse = malloc(sizeof(t_parse));
-    if (!parse)
-    {
-        // perror("malloc");
-       perror("\033[31mError");
-        exit(EXIT_FAILURE);
-    }
+	t_parse	*parse;
+	char	**str;
+	int		num_tokens;
 
-    parse->input = input;
-    parse->file = ft_strdup(file);
-    if (!parse->file)
-        perror_exit(parse);
-
-    char **str = ft_split(commands, ' ');
-    if (!str)
-        perror_exit(parse);
-
-    parse->cmd = ft_strdup(str[0]);
-    if (!parse->cmd)
-        perror_exit(parse);
-int num_tokens = 0;
-    for (int i = 0; str[i]; i++)
-        num_tokens++;
-parse->args = malloc(sizeof(char *) * (num_tokens + 1));
-for(int i = 0; str[i]; i++)
-{
-    parse->args[i] = ft_strdup(str[i]);
-    if (!parse->args[i])
-        perror_exit(parse);
-}
-parse->args[num_tokens] = NULL;
-
-    return parse;
+	parse = malloc(sizeof(t_parse));
+	if (!parse)
+	{
+		// perror("malloc");
+		perror("\033[31mError");
+		exit(EXIT_FAILURE);
+	}
+	parse->input = input;
+	parse->file = ft_strdup(file);
+	if (!parse->file)
+	{
+		free(parse);
+		perror_exit();
+	}
+	str = ft_split(commands, ' ');
+	if (!str)
+	{
+		free(parse);
+		perror_exit();
+	}
+	parse->cmd = ft_strdup(str[0]);
+	if (!parse->cmd)
+	{
+		free(parse);
+		perror_exit();
+	}
+	num_tokens = 0;
+	for (int i = 0; str[i]; i++)
+		num_tokens++;
+	parse->args = malloc(sizeof(char *) * (num_tokens + 1));
+	for (int i = 0; str[i]; i++)
+	{
+		parse->args[i] = ft_strdup(str[i]);
+		if (!parse->args[i])
+		{
+			free(parse);
+			perror_exit();
+		}
+	}
+	parse->args[num_tokens] = NULL;
+	return (parse);
 }
--- pipex.c
+++ pipex.c
@@ -13,238 +13,217 @@
 #include "pipex.h"
 #include <errno.h>
 
-void error(void)
+void	error(void)
 {
-    // perror("\033[31mError");
-    strerror(errno);
-    exit(EXIT_FAILURE);
+	// perror("\033[31mError");
+	strerror(errno);
+	exit(EXIT_FAILURE);
 }
-int exec_permissions_added(char *path)
+// void	error(const char *message)
+// {
+// 	perror(message);
+// 	exit(EXIT_FAILURE);
+// }
+
+int	exec_permissions_added(char *path)
 {
-    int acc = access(path, X_OK);
-    if (acc == 0)
-    {
-        unlink(path);
-        return 1;
-    }
-    return 0;
-}
-
-char *find_command_path(char *cmd, char **envp)
-{
-    char *path_env = NULL;
-    char **paths = NULL;
-    char *full_path = NULL;
-    int i = 0;
-
-    while (envp[i])
-    {
-        if (ft_strncmp(envp[i], "PATH=", 5) == 0)
-        {
-            path_env = envp[i] + 5;
-            break;
-        }
-        i++;
-    }
-
-    if (!path_env)
-        return NULL;
-
-    paths = ft_split(path_env, ':');
-    if (!paths)
-    {
-        // perror("ft_split");
-
-        exit(1);
-    }
-
-    for (i = 0; paths[i]; i++)
-    {
-        full_path = ft_strjoin(paths[i], "/");
-        if (!full_path)
-        {
-            free(paths);
-            // perror("ft_strjoin");
-
-            exit(1);
-        }
-
-        char *temp = ft_strjoin(full_path, cmd);
-        free(full_path);
-        full_path = temp;
-
-        if (!full_path)
-        {
-            free(paths);
-            // perror("ft_strjoin");
-
-            exit(1);
-        }
-
-        if (exec_permissions_added(full_path) == 1)
-        {
-            for (int j = 0; paths[j]; j++)
-                free(paths[j]);
-            free(paths);
-            return full_path;
-        }
-
-        free(full_path);
-        full_path = NULL;
-    }
-
-    for (i = 0; paths[i]; i++)
-        free(paths[i]);
-    free(paths);
-
-    return NULL;
+	return (access(path, X_OK) == 0);
 }
 
-int ft_in_out(char *file, int mode)
+char	*find_command_path(char *cmd, char **envp)
 {
-    int fd;
-    if (mode == 0)
-    {
-        fd = open(file, O_RDONLY);
-        if (fd < 0)
-        {
-            // perror("open");
+	char	*path_env;
+	char	**paths;
+	char	*full_path;
+	int		i;
+	char	*temp;
 
-            return -1;
-        }
-    }
-    else if (mode == 1)
-    {
-        fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (fd < 0)
-        {
-            // perror("open");
-
-            return -1;
-        }
-    }
-    else
-    {
-        fd = -1;
-    }
-    return fd;
+	path_env = NULL;
+	paths = NULL;
+	full_path = NULL;
+	for (i = 0; envp[i]; i++)
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			path_env = envp[i] + 5;
+			break ;
+		}
+	}
+	if (!path_env)
+	{
+		fprintf(stderr, "PATH environment variable not found\n");
+		return (NULL);
+	}
+	paths = ft_split(path_env, ':');
+	if (!paths)
+		error();
+	for (i = 0; paths[i]; i++)
+	{
+		temp = ft_strjoin(paths[i], "/");
+		if (!temp)
+		{
+			for (int j = 0; paths[j]; j++)
+				free(paths[j]);
+			free(paths);
+			error();
+		}
+		full_path = ft_strjoin(temp, cmd);
+		free(temp);
+		if (!full_path)
+		{
+			for (int j = 0; paths[j]; j++)
+				free(paths[j]);
+			free(paths);
+			error();
+		}
+		if (exec_permissions_added(full_path))
+		{
+			for (int j = 0; paths[j]; j++)
+				free(paths[j]);
+			free(paths);
+			return (full_path);
+		}
+		unlink(full_path);
+		free(full_path);
+		full_path = NULL;
+	}
+	for (i = 0; paths[i]; i++)
+		free(paths[i]);
+	free(paths);
+	fprintf(stderr, "Command not found: %s\n", cmd);
+	return (NULL);
 }
 
-void pipe_and_fork(t_cmd *cmd, char **envp)
+int	ft_in_out(char *file, int mode)
 {
-    char *cmd_path = NULL;
-    int pipefd[2];
-    if (pipe(pipefd) == -1)
-    {
-        // perror("pipe");
+	int	fd;
 
-        error();
-    }
-    pid_t pid = fork();
-    if (pid == -1)
-    {
-        // perror("fork");
-
-        error();
-    }
-
-    if (pid == 0)
-    {
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[0]);
-        close(pipefd[1]);
-
-        cmd_path = find_command_path(cmd->parse[0]->cmd, envp);
-        if (!cmd_path)
-        {
-            // perror("Command not found");
-            //
-            error();
-        }
-
-        if (execve(cmd_path, cmd->parse[0]->args, envp) == -1)
-        {
-
-            error();
-        }
-        // perror("execve");
-
-        free(cmd_path);
-        error();
-    }
-    else
-    {
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
-        close(pipefd[1]);
-
-        waitpid(pid, NULL, 0);
-
-        cmd_path = find_command_path(cmd->parse[1]->cmd, envp);
-        if (!cmd_path)
-        {
-            // printf("Command not found: %s\n", cmd->parse[1]->cmd);
-            // ft_printf("Command not found", strerror(errno));
-
-            error();
-        }
-        execve(cmd_path, cmd->parse[1]->args, envp);
-        // perror("execve");
-
-        free(cmd_path);
-        // printf("Error: %s\n", strerror(errno));
-
-        error();
-    }
+	if (mode == 0)
+	{
+		fd = open(file, O_RDONLY);
+		if (fd < 0)
+		{
+			// perror("open");
+			return (-1);
+		}
+	}
+	else if (mode == 1)
+	{
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd < 0)
+		{
+			// perror("open");
+			return (-1);
+		}
+	}
+	else
+	{
+		fd = -1;
+	}
+	return (fd);
 }
 
-int main(int argc, char **argv, char **envp)
+void	pipe_and_fork(t_cmd *cmd, char **envp)
 {
-    if (argc != 5)
-    {
-        // perror("Usage: ./pipex file1 cmd1 cmd2 file2");
+	char	*cmd_path;
+	int		pipefd[2];
+	pid_t	pid;
 
-        error();
-    }
-    t_cmd *cmd = malloc(sizeof(t_cmd));
-    if (!cmd)
-    {
-        // perror("malloc");
+	cmd_path = NULL;
+	if (pipe(pipefd) == -1)
+	{
+		// perror("pipe");
+		error();
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		// perror("fork");
+		error();
+	}
+	if (pid == 0)
+	{
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		// Find the command path
+		cmd_path = find_command_path(cmd->parse[0]->cmd, envp);
+		if (!cmd_path)
+		{
+			error();
+		}
+		if (execve(cmd_path, cmd->parse[0]->args, envp) == -1)
+		{
+			free(cmd_path);
+			error();
+		}
+		free(cmd_path);
+		error();
+	}
+	else
+	{
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		waitpid(pid, NULL, 0);
+		cmd_path = find_command_path(cmd->parse[1]->cmd, envp);
+		if (!cmd_path)
+		{
+			error();
+		}
+		if (execve(cmd_path, cmd->parse[1]->args, envp) == -1)
+		{
+			free(cmd_path);
+			error();
+		}
+		free(cmd_path);
+		error();
+	}
+}
 
-        error();
-    }
+int	main(int argc, char **argv, char **envp)
+{
+	t_cmd	*cmd;
+	int		in_fd;
+	int		out_fd;
 
-    cmd->parse[0] = init_parse(argv[1], argv[2], true);
-    cmd->parse[1] = init_parse(argv[4], argv[3], false);
-    cmd->fd_in = argv[1];
-    cmd->fd_out = argv[4];
-    int in_fd = ft_in_out(cmd->fd_in, 0);
-    if (in_fd == -1)
-    {
-        free(cmd->parse[0]);
-        free(cmd->parse[1]);
-        free(cmd);
-
-        error();
-    }
-    int out_fd = ft_in_out(cmd->fd_out, 1);
-    if (out_fd == -1)
-    {
-        free(cmd->parse[0]);
-        free(cmd->parse[1]);
-        free(cmd);
-
-        error();
-    }
-    dup2(in_fd, STDIN_FILENO);
-    dup2(out_fd, STDOUT_FILENO);
-    pipe_and_fork(cmd, envp);
-    close(in_fd);
-    close(out_fd);
-
-    free(cmd->parse[0]);
-    free(cmd->parse[1]);
-    free(cmd);
-
-    return 0;
+	if (argc != 5)
+	{
+		error();
+	}
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+	{
+		// perror("malloc");
+		error();
+	}
+	cmd->parse[0] = init_parse(argv[1], argv[2], true);
+	cmd->parse[1] = init_parse(argv[4], argv[3], false);
+	cmd->fd_in = argv[1];
+	cmd->fd_out = argv[4];
+	in_fd = ft_in_out(cmd->fd_in, 0);
+	if (in_fd == -1)
+	{
+		free(cmd->parse[0]);
+		free(cmd->parse[1]);
+		free(cmd);
+		error();
+	}
+	out_fd = ft_in_out(cmd->fd_out, 1);
+	if (out_fd == -1)
+	{
+		free(cmd->parse[0]);
+		free(cmd->parse[1]);
+		free(cmd);
+		error();
+	}
+	dup2(in_fd, STDIN_FILENO);
+	dup2(out_fd, STDOUT_FILENO);
+	pipe_and_fork(cmd, envp);
+	close(in_fd);
+	close(out_fd);
+	free(cmd->parse[0]);
+	free(cmd->parse[1]);
+	free(cmd);
+	return (0);
 }
